c17beffafcd3679bc876bd3965467eab496d7308
Vicente Palma <vicente.palma2301@alumnos.ubiobio.cl>
2026-09-12 00:35:05 -0300

    fix: align prisma migrations with schema by dropping fotoUrl column

---
+++ backend/prisma/migrations/20260912033402_remove_fotourl/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `fotoUrl` on the `animales` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "animales" DROP COLUMN "fotoUrl";
